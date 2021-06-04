48b1bd7250fc63c66db5d7a8c4fdceac1a5eae93
Sebastián <sebastian.arancibia@alumnos.ucn.cl>
2021-06-04 16:05:54 -0400

    feat(MainActivity): inflate XML

--- app/src/main/java/cl/ucn/disc/dsm/sarancibia/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/sarancibia/MainActivity.java
@@ -1,14 +1,52 @@
+/*
+ * Copyright (c) 2021. Sebastián Arancibia-Arzic
+ */
+
 package cl.ucn.disc.dsm.sarancibia;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import cl.ucn.disc.dsm.sarancibia.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * The Main class.
+ *
+ * @author Sebastian Arancibia
+ */
+public final class MainActivity extends AppCompatActivity {
 
+    /**
+     * The Logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(MainActivity.class);
+
+    /**
+     * The Bindings
+     */
+    private ActivityMainBinding binding;
+
+    /**
+     * @param savedInstanceState the instance.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Inflate the xml
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(this.binding.getRoot());
+
+        // Set the toolbar
+        this.setSupportActionBar(this.binding.toolbar);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        log.debug("onStart ..");
+
+    }
+
 }
